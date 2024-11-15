a549501ac4c2666f1214d860935a2b3e80db9c72
Mahmudbek <mahmudbekkjumayev@gmail.com>
2024-11-15 10:01:50 +0500

    Table scrores

--- MainDirect/sheets.py
+++ MainDirect/sheets.py
@@ -1,26 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, db
+from tabulate import tabulate
 
-
-cred = credentials.Certificate("./pdp-projectquiz-firebase-adminsdk-a0msr-34c1ab9e5e.json")  
+# Firebase’ga ulanish
+cred = credentials.Certificate("./pdp-projectquiz-firebase-adminsdk-a0msr-34c1ab9e5e.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://your-database-name.firebaseio.com'  
+    'databaseURL': 'https://pdp-projectquiz.firebaseio.com'
 })
 
-
-
-async def update_firebase_score(username, correct_count):
+def get_scores():
     try:
-        user_ref = db.reference(f'scores/{username}')
-        current_score = user_ref.get() or 0
-        user_ref.set(current_score + correct_count)
         scores_ref = db.reference('scores')
         all_data = scores_ref.get()
         sorted_data = sorted(all_data.items(), key=lambda x: x[1], reverse=True)
-        
-        print("Reyting:")
+        table_data = [["Username", "Score"]]
         for username, score in sorted_data:
-            print(f"{username}: {score}")
+            table_data.append([username, score])
+
+        print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Xatolik: {e}")
+
+get_scores()
