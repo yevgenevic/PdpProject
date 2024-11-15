937104feb4c9137b03d6208eb8956ff29e047c95
Mahmudbek <mahmudbekkjumayev@gmail.com>
2024-11-15 09:54:00 +0500

    Real time firebase

--- MainDirect/sheets.py
+++ MainDirect/sheets.py
@@ -1,50 +1,26 @@
-import gspread_asyncio
-from oauth2client.service_account import ServiceAccountCredentials
-
-SPREADSHEET_ID = '1n-2xd1BOjZdg3kuy2pdg0t_MTyjJ87BV5Gomk3Xsje0'
-SHEET_NAME = 'PDP'
+import firebase_admin
+from firebase_admin import credentials, db
 
 
-def auth_gspread():
-    scope = [
-        'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        '/home/yevgenevic/PycharmProjects/PdpProject/MainDirect/botproject-400713-67dc83c63c0d.json', scope)
-    return creds
+cred = credentials.Certificate("./pdp-projectquiz-firebase-adminsdk-a0msr-34c1ab9e5e.json")  
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://your-database-name.firebaseio.com'  
+})
 
 
-async def connect_to_google_sheets():
-    agcm = gspread_asyncio.AsyncioGspreadClientManager(auth_gspread)
-    agc = await agcm.authorize()
-    sh = await agc.open_by_key(SPREADSHEET_ID)
-    return await sh.worksheet(SHEET_NAME)
 
-
-async def update_google_sheet(username, correct_count):
+async def update_firebase_score(username, correct_count):
     try:
-        sheet = await connect_to_google_sheets()
-        cell = await sheet.find(username)
+        user_ref = db.reference(f'scores/{username}')
+        current_score = user_ref.get() or 0
+        user_ref.set(current_score + correct_count)
+        scores_ref = db.reference('scores')
+        all_data = scores_ref.get()
+        sorted_data = sorted(all_data.items(), key=lambda x: x[1], reverse=True)
+        
+        print("Reyting:")
+        for username, score in sorted_data:
+            print(f"{username}: {score}")
 
-        if cell:
-            current_count_value = await sheet.cell(cell.row, cell.col + 1)
-            current_count = 0
-            if current_count_value.value:
-                current_count = int(current_count_value.value)
-            await sheet.update_cell(cell.row, cell.col + 1, current_count + correct_count)
-        else:
-            await sheet.append_row([username, correct_count])
-
-        all_data = await sheet.get_all_values()
-        sorted_data = sorted(
-            [row for row in all_data[1:] if row[1].isdigit()],
-            key=lambda x: int(x[1]),
-            reverse=True
-        )
-        await sheet.clear()
-        await sheet.append_row(['Username', 'Score'])
-        for row in sorted_data:
-            await sheet.append_row(row)
     except Exception as e:
         print(f"An error occurred: {e}")
